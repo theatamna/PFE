cdbde8d0355316c2309d8bcaa47554ee09e24c36
theatamna <60579369+theatamna@users.noreply.github.com>
2020-03-11 11:34:02 +0100

    Added normalization function

--- dummy.py
+++ dummy.py
@@ -1,6 +1,11 @@
 import torch
 from torch.utils.data import TensorDataset
 
+def Normalize_Adj(A): # may need to use torch.repeat and torch.bmm
+  A_tilda = A + torch.eye(A.shape[1])
+  D_tilda = torch.diag(torch.sum(A, 2).pow(-0.5))
+  A_hat = D_tilda.mm(A_tilda).mm(D_tilda)
+  return A_hat
 def get_dataset(n_train=1024, n_valid=1024, n_nodes=5):
     A = torch.randint(2, [n_train + n_valid, n_nodes, n_nodes])
     upper_tr = torch.triu(A, diagonal=1)
