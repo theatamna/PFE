a867fda70db816c3c1c2ec6b4c69182d23ca1f19
waretf <ibrahim.laiche@gmail.com>
2020-03-14 19:39:09 +0100

    added labels to dummy data generator

--- dummy.py
+++ dummy.py
@@ -8,18 +8,23 @@ def Normalize_Adj(A): # may need to use torch.repeat and torch.bmm
     A_hat = D_tilda.bmm(A_tilda).bmm(D_tilda)
     return A_hat
 
-def get_dataset(n_train=1024, n_valid=1024, n_nodes=5):
+def get_dataset(n_train=1024, n_valid=1024, n_nodes=5, n_classes=4, target_shape=[]):
+    # Generate random adjacency matrices
     A = torch.randint(2, [n_train + n_valid, n_nodes, n_nodes])
     A = A.to(dtype)
     upper_tr = torch.triu(A, diagonal=1)
     data =  upper_tr + torch.transpose(upper_tr, 1, 2)
-    data = Normalize_Adj(data)
+    data = Normalize_Adj(data) # Normalization
     data = torch.split(data, split_size_or_sections=[n_train, n_valid], dim=0)
-    train_data = TensorDataset(data[0])
-    valid_data = TensorDataset(data[1])
+    # Generating labels
+    train_y = torch.randint(n_classes, [n_train] + target_shape, dtype=dtype)
+    valid_y = torch.randint(n_classes, [n_valid] + target_shape, dtype=dtype)
+
+    train_data = TensorDataset(data[0], train_y)
+    valid_data = TensorDataset(data[1], valid_y)
     return train_data, valid_data
 
 def _test():
-    t, v = get_dataset(100, 100)
+    t, v = get_dataset(5, 2)
     for d in t.tensors + v.tensors:
-        print(d.size())
+        print(d)
