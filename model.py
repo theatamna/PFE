a2f5fa10d5e7f52eb3fcfa7a097d319b58be361d
theatamna <60579369+theatamna@users.noreply.github.com>
2020-03-16 19:57:12 +0100

    Fixed GCN model

--- model.py
+++ model.py
@@ -4,25 +4,33 @@ import torch.nn.functional as F
 
 dtype = torch.float
 
+class GraphConvolutionLayer(nn.Module):
+  def __init__(self, input_dim, output_dim):
+    super().__init__()
+    self.fc = nn.Linear(input_dim, output_dim, bias=False)
+    torch.nn.init.xavier_uniform_(self.fc.weight)
+  def forward(self, X, A):
+    out = self.fc(X)
+    out = torch.bmm(A, out)  
+    return out
+
 class TwoLayerGCN(nn.Module):
-    '''
-    '''
-    def __init__(self, Dim):
-        super().__init__()
-        self.fc1 = nn.Linear(Dim[0][0], Dim[0][1], bias=False)
-        self.fc2 = nn.Linear(Dim[1][0], Dim[1][1], bias=False)
-        torch.nn.init.xavier_uniform_(self.fc1.weight)
-        torch.nn.init.xavier_uniform_(self.fc2.weight)
+  def __init__(self, input_dim=n_nodes, hidden_dim=hidden_dim, n_classes=n_classes, dropout=dropout):
+    super().__init__()
+    self.gc1 = GraphConvolutionLayer(input_dim, hidden_dim)
+    self.gc2 = GraphConvolutionLayer(hidden_dim, n_classes)
+    self.dropout = dropout
 
-    def forward(self, A, X):
-        out = self.fc1(torch.mm(A,X))
-        out = F.relu(out)
-        out = self.fc2(torch.mm(A,out))
-        out = F.softmax(out) 
-        return out
+  def forward(self, X, A):
+    X = X.repeat(A.shape[0], 1, 1)
+    #print('X: ', X.shape)
+    out = self.gc1(X, A)
+    #print('Output after 1st GCN layer: ', out.shape)
+    out = F.relu(out)
+    out = F.dropout(out, self.dropout)
+    out = self.gc2(out, A)
+    #print('Output after 2nd GCN layer: ', out.shape)
+    #out = F.softmax(out) 
+    return out
 
-# A = torch.tensor([[1,0,1,0],[0,1,1,0],[1,1,1,0],[0,0,0,1]], dtype=dtype)
-# X = torch.eye(4, dtype=dtype)
-
-# Dim = [[4,10],[10,5]]
 
