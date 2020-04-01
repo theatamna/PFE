4d3ca2e156808f67635004736706275d6f46e835
IbrahimL <ibrahim.laiche@g.enp.edu.dz>
2020-04-01 19:07:25 +0100

    I think it's done

--- get_dort_graphs.py
+++ get_dort_graphs.py
@@ -6,6 +6,7 @@ def get_dort_graphs(ds_name):
     "WORK IN PROGRESS"
     adjacency_matrices = []
     features_matrices = []
+    nodes_in_graph = []
     data_dir = "./data/"
     edges = np.array([(x[0], x[1]) for x in np.loadtxt(data_dir + ds_name + '/' + ds_name + "_A.txt",
                      delimiter = ',', dtype = 'int')])
@@ -13,7 +14,15 @@ def get_dort_graphs(ds_name):
     graph_labels = (graph_labels > 0) * 1
     ind = np.loadtxt(data_dir + ds_name + '/' + ds_name + "_graph_indicator.txt", dtype = 'int')
     _, indices = np.unique(ind, return_index = True)
-    #
+
+    # Getting node labels
+    try:
+        nodes_label = np.split(np.loadtxt(data_dir + ds_name + '/' + ds_name + "_node_labels.txt", dtype = 'int'),
+                               indices[1:])
+    except OSError:
+        nodes_label = None
+
+    # Getting adjacency matrices
     n_nodes = len(ind)
     n_graphs = ind[-1]
     G = nx.Graph()
@@ -22,12 +31,23 @@ def get_dort_graphs(ds_name):
     for i in range(1, n_graphs):
         G_sub = G.subgraph(range(indices[i-1] + 1, indices[i] + 1))
         adjacency_matrices.append(np.array(nx.to_numpy_matrix(G_sub)))
+        nodes_in_graph.append(G_sub.number_of_nodes())
     G_sub = G.subgraph(range(indices[-1] + 1, n_nodes + 1))
     adjacency_matrices.append(np.array(nx.to_numpy_matrix(G_sub)))
-    return adjacency_matrices, graph_labels
+    nodes_in_graph.append(G_sub.number_of_nodes())
+
+    # Getting feature matrices
+    try:
+        node_features = np.loadtxt(data_dir + ds_name + '/' + ds_name + "_node_attributes.txt", delimiter = ',')
+        for i in range(1, n_graphs):
+            features_matrices.append(node_features[indices[i-1] + 1: indices[i] + 1])
+        features_matrices.append(node_features[indices[-1] + 1: n_nodes + 1])
+    except OSError:
+        for num in nodes_in_graph:
+            features_matrices.append(np.eye(num))
+
+    return adjacency_matrices, graph_labels, features_matrices, nodes_label
 
 
-
-
-ds_name = "PTC_MR"
-K = get_dort_graphs(ds_name)
+ds_name = "PROTEINS_full"
+K, B, F, C = get_dort_graphs(ds_name)
