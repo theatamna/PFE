b7d3746bc24cb13b06c19be35ec84fd562952497
theatamna <60579369+theatamna@users.noreply.github.com>
2020-04-05 15:24:42 +0100

    Minor changes

--- train.py
+++ train.py
@@ -28,21 +28,21 @@ def train_GNN(model, train_loader, optimizer, criterion, num_epochs, device):
 
             # Forward pass
             _, outputs = model(Adj, Feat)
-            loss = criterion(outputs.transpose(1,2), labels)
+            loss = criterion(outputs.transpose(1, 2), labels)
 
             # Backward and optimize
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            _, predicted = torch.max(outputs,2)
+            _, predicted = torch.max(outputs, 2)
             correct += (predicted == labels).sum()
             total += labels.numel()
 
         train_log[epoch, 0] = epoch
         train_log[epoch, 1] = loss.item()
-        print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss))
-        train_log[epoch, 2] = (100* correct / total)
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss))
+        train_log[epoch, 2] = (100 * correct / total)
 
     train_log = train_log.detach().cpu().numpy()
     return train_log
@@ -51,8 +51,8 @@ def plot_learning_curves(train_log):
     fig, ax = plt.subplots(2, 1, figsize=(10, 10))
     fig.tight_layout()
 
-    ax[0].plot(train_log[:,0], train_log[:,1])
+    ax[0].plot(train_log[:, 0], train_log[:, 1])
     ax[0].set(xlabel="epochs", ylabel="loss")
 
-    ax[1].plot(train_log[:,0], train_log[:,2])
+    ax[1].plot(train_log[:, 0], train_log[:, 2])
     ax[1].set(xlabel="epochs", ylabel="train_acc")
