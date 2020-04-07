cc19465ab7ddf86d370fb4e5ae85bd86fc873a7c
IbrahimL <ibrahim.laiche@g.enp.edu.dz>
2020-04-07 11:46:52 +0100

    change attention_layer to output attention scores

--- models/layers.py
+++ models/layers.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_sequence
 
+dtype = torch.float32
+torch.set_default_tensor_type(torch.FloatTensor)
+
 class GraphConvolutionLayer(nn.Module):
   def __init__(self, input_dim, output_dim):
     super().__init__()
@@ -50,11 +53,11 @@ class attention_layer(nn.Module):
         self.out_features = out_features
         self.alpha = alpha
         self.dropout = dropout
-        self.non_lin = non_lin
+        #self.non_lin = non_lin
 
         gain = torch.nn.init.calculate_gain('leaky_relu', alpha)
 
-        self.fc = nn.Linear(in_features, out_features, bias=False) # First linear transformation
+        #self.fc = nn.Linear(in_features, out_features, bias=False) # First linear transformation
         self.a = nn.Parameter(torch.zeros((2*out_features, 1)))
         nn.init.xavier_uniform_(self.a.data, gain=gain)
 
@@ -79,19 +82,9 @@ class attention_layer(nn.Module):
 
     def forward(self, batch_graphs, batch_features):
         batch_size, n_nodes, _ = batch_graphs.shape
+        attention_scores = torch.zeros_like(batch_graphs)
 
-        # Reshape features to pass them to linear layer
-        mod_features = batch_features.reshape(batch_features.shape[0]*batch_features.shape[1], -1)
-
-        mod_features = F.dropout(mod_features, self.dropout) # Dunno if this is correct)
-
-        # Linear transformation (dimension of features: in_features --> out_features)
-        mod_features = self.fc(mod_features)
-
-        # Back to the original batched shape
-        mod_features = mod_features.reshape(batch_features.shape[0], batch_features.shape[1], -1)
-
-        concat_features = self.node_neighbors_features(batch_graphs, mod_features)
+        concat_features = self.node_neighbors_features(batch_graphs, batch_features)
         scores = self.LeakyRelu(torch.mm(concat_features, self.a))
 
         # Split the output: each chunk contains attention coefficients for a single node
@@ -100,13 +93,7 @@ class attention_layer(nn.Module):
         scores = pad_sequence(scores, batch_first=True, padding_value=-9e15)
         scores = F.softmax(scores).flatten()
         scores = scores[scores>0]
-        scores = F.dropout(scores, self.dropout)
-        out = scores.unsqueeze(1)*concat_features[:,self.out_features:]
-        out = pad_sequence(torch.split(out, degrees.tolist(), dim=0),
-                                        batch_first=True,
-                                        padding_value=0)
-        out = torch.sum(out, dim=1)
-        out = out.reshape(batch_size, n_nodes, -1)
-        if self.non_lin != False:
-            out = self.non_lin(out)
-        return out
+        #scores = F.dropout(scores, self.dropout)
+        attention_scores.masked_scatter_(batch_graphs.to(torch.bool), scores)
+
+        return attention_scores
