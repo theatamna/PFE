48eafc61f9a40b6a88682d46260acd45e04343a9
IbrahimL <ibrahim.laiche@g.enp.edu.dz>
2020-04-16 15:08:25 +0000

    fixed some bugs

--- prep_dataset.py
+++ prep_dataset.py
@@ -10,8 +10,9 @@ def prep_dataset(ds_name):
     adjacency_matrices, graph_labels, features_matrices, nodes_label = get_dort_graphs(ds_name)
     nb_max_nodes = max(a.shape[0] for a in adjacency_matrices) # Max no. of nodes in a single graph
     d_max = max(x.shape[1] for x in features_matrices) # Max no. of features (different from graph to graph only when node features aren't available)
-    n_classes = max(max(x) for x in nodes_label) + 1
-    info = [d_max, n_classes] # Number of nodes and number of classes
+    n_node_classes = max(max(x) for x in nodes_label) + 1
+    n_graph_classes = max(graph_labels) + 1
+    info = [d_max, n_graph_classes, n_node_classes] # Number of nodes and number of classes
 
     n_graphs = len(adjacency_matrices)
 
